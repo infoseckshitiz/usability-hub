e7df775f7eeb1875fed3f892d095e984741ef2e6
infoseckshitiz <infoseckshitizkhadka@gmail.com>
2024-06-02 23:24:33 +0545

    features card section 1st staging

---
+++ basicjslearn.js
@@ -0,0 +1,86 @@
+// console.log("hello kshitiz")
+// console.log("hello kshitiz")
+
+// num = 2;
+// num2 = 2;
+// sum = num + num2
+// console.log(sum)
+
+// // alert("hello java script");
+
+// console.log("This is my first time on javascript \n i am very happy to code on java script");
+// console.log("my name is " + "Khsitiz Khadka");
+// console.log('half of 100 is ${100/2}');
+// console.log(typeof 4.5)
+// console.log(typeof "X")
+// console.log(-(10 -2))
+// console.log(3 > 2)
+// console.log(3<2)
+// console.log("Z"<"a")
+// console.log("a"=="a")
+// console.log("a"=="b")
+// console.log(true && false)
+// console.log(true && true)
+// console.log(false ||true)
+// console.log(false || false)
+// console.log(null)
+// console.log(8*null)
+// console.log("5"-1)
+// console.log("5" + 1) //in this code it output will be 51 because js read + as concat
+// console.log("five" * 2)
+
+// let ten = 10
+// console.log(ten * ten)
+
+// let mood = "light";
+// console.log(mood)
+
+// mood = "dark";
+// console.log(mood)
+
+// let one = 1 , tow = 2;
+// console.log(one + tow)
+
+// var hy = "Alisha \t";
+// const greet = "hello\t";
+// console.log(greet + hy)
+
+// console.log(Math.min(2,4))
+
+// let theNumber = Number(prompt("Pick a number "));
+// console.log("Your number is the square root of " + theNumber * theNumber)
+// let number = Number(prompt("pick a number"));
+// let number1 = Number(prompt("pick a number"));
+// console.log("the sum of number is " + (number + number1))
+
+// if(1+ 1 == 2){
+//     console.log("Its true:")
+// }
+// else{
+//     console.log("its a false")
+// }
+
+// if(2 != 2){
+//     console.log("Its' true")
+// }
+// else{
+//     console.log("its a false")
+// }
+
+// write a program to find  the grater and smaller number
+// let num = 2
+// let num1 = 4
+// if (num > num1){
+//     console.log("num  is grater then num1")
+// }
+// else{
+//     console.log("num1 is grater then num")
+// }
+
+// let number = 0;
+// while (number >= 12){
+//     console.log(number);
+//     number == number + 2;
+// }
+
+console.log("hello");
